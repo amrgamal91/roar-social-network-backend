cb4757d2cec3cac1d8d459f776a165c014c7764c
gemy91 <ag.amrgamal91@gmail.com>
2019-07-14 14:45:54 +0300

    new feature - set/get authenticated user details

--- dbschema.js
+++ dbschema.js
@@ -1,5 +1,17 @@
 //this file is just a reference for db schema
 let db = {
+  users: [
+    {
+      userId: "ksdoa912ejdwqijoijwqoi",
+      emai: "user@email.com",
+      handle: "user",
+      createdAt: "2019-07-10T03:52:32.227Z",
+      imageUrl: "image/dsfsdfkasjfk/fsaasfswq",
+      bio: "hello my name is user , nice to meet you ",
+      website: "https://user.com",
+      location: "London,UK"
+    }
+  ],
   screams: [
     {
       userHandle: "user",
@@ -7,6 +19,30 @@ let db = {
       createdAt: "2019-07-10T03:52:32.227Z",
       likeCount: 5,
       commentCount: 2
+    }
+  ]
+};
+
+const userDetails = {
+  // Redux data
+  credentials: {
+    userId: "N43KJ5H43KJHREW4J5H3JWMERHB",
+    email: "user@email.com",
+    handle: "user",
+    createdAt: "2019-03-15T10:59:52.798Z",
+    imageUrl: "image/dsfsdkfghskdfgs/dgfdhfgdh",
+    bio: "Hello, my name is user, nice to meet you",
+    website: "https://user.com",
+    location: "Lonodn, UK"
+  },
+  likes: [
+    {
+      userHandle: "user",
+      screamId: "hh7O5oWfWucVzGbHH2pa"
+    },
+    {
+      userHandle: "user",
+      screamId: "3IOnFoQexRcofs5OhBXO"
     }
   ]
 };
--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,23 @@ const app = require("express")();
 const FBAuth = require("./util/fbAuth");
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
 
 //scream routes
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
+app.post("/user/image", FBAuth, uploadImage);
+app.post("/user", FBAuth, addUserDetails);
+app.get("/user", FBAuth, getAuthenticatedUser);
 
 //users routes
 app.post("/signup", signup);
 app.post("/login", login);
-app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
