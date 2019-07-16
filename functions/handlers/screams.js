4ef244cd3c153451b9974de0252ac60d59b6e711
gemy91 <ag.amrgamal91@gmail.com>
2019-07-16 12:07:22 +0300

    new features (triggers) - onScreamDelete , onChangeImage

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -11,7 +11,10 @@ exports.getAllScreams = (req, res) => {
           screamId: doc.id,
           body: doc.data().body,
           userHandle: doc.data().userHandle,
-          createdAt: doc.data().createdAt
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+          userImage: doc.data().userImage
         });
       });
       return res.json(screams);
@@ -80,7 +83,7 @@ exports.getScream = (req, res) => {
 //comment on a scream
 exports.commentOnScream = (req, res) => {
   if (req.body.body.trim() == "")
-    return res.status(400).json({ error: "Must not be empty" });
+    return res.status(400).json({ comment: "Must not be empty" });
 
   const newComment = {
     body: req.body.body,
